a5e167f80668ca03a0d2a12b697c25e9b66fe533
VinceGonzales <vince.gonzales@hotmail.com>
2018-08-13 12:06:31 -0700

    type fix

--- Xolartek.Kendo/Xolartek.Web/Scripts/typescripts/utils.ts
+++ Xolartek.Kendo/Xolartek.Web/Scripts/typescripts/utils.ts
@@ -6,7 +6,7 @@
         }
         public GenerateGUID(): string {
             var result: string = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c: any): string {
-                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                var r: number = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
                 return v.toString(16);
             });
             return result;
