72535cc3870b12d47ff6f09c69bfa0dd39e3db8e
VinceGonzales <vince.gonzales@hotmail.com>
2018-09-12 13:43:04 -0700

    Hero page fix

---
+++ Xolartek.Kendo/Xolartek.Web/Scripts/fortnite/Index.js
@@ -0,0 +1,52 @@
+﻿var skillDTO = {
+    id: { type: "number" },
+    name: { type: "string" },
+    classname: { type: "string" },
+    description: { type: "string" },
+    heroname: { type: "string" },
+    issupport: { type: "boolean" },
+    istactical: { type: "boolean" }
+};
+var skillModel = kendo.data.Model.define({ id: "Id", fields: skillDTO });
+
+function StarLabel(data) {
+    var result = '';
+    for (var i = 0; i < data; i++) {
+        result += "<span class='k-icon k-i-custom'></span>";
+    }
+    return result;
+}
+function onGridChange(e) {
+    var grid = e.sender;
+    var selected = $.map(this.select(), function (item) {
+        return grid.dataItem(item);
+    });
+    var apiUrl = "http://localhost:50090/api/Fortnite/Heroes/" + selected[0].Id;
+    var dsHero = new kendo.data.DataSource({
+        autoSync: true,
+        transport: {
+            read: { dataType: "json", type: "get", url: apiUrl }
+        },
+        schema: { data: "Skills", model: skillModel }
+    });
+    $("#pnlGrid").kendoGrid({
+        dataSource: dsHero,
+        columns: [
+            { width: 80, field: "id", title: " ", hidden: true },
+            { width: 80, field: "name", title: "Skill" },
+            { width: 80, field: "description", title: "Description" }
+        ],
+        dataBound: function (e) {
+            var rows = e.sender.tbody.children();
+            for (var j = 0; j < rows.length; j++) {
+                var row = $(rows[j]);
+                var dataItem = e.sender.dataItem(row);
+                var isTactical = dataItem.get("istactical");
+                if (isTactical) {
+                    row.addClass("row-tactical");
+                }
+            }
+        },
+        height: 500
+    });
+}
