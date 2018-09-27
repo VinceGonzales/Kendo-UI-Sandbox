dc24f871af4ee9f5db130d6462268cbe722a2e0a
VinceGonzales <vince.gonzales@hotmail.com>
2018-09-27 09:45:45 -0700

    Updates for data entry

--- Xolartek.Kendo/Xolartek.Web/Areas/Fortnite/Models/Repository.cs
+++ Xolartek.Kendo/Xolartek.Web/Areas/Fortnite/Models/Repository.cs
@@ -219,7 +219,7 @@ namespace Xolartek.Web.Fortnite.Models
         }
         #endregion
 
-        private void AddTraitImpact(string value, int schemaId, int traitId)
+        public void AddTraitImpact(string value, int schemaId, int traitId)
         {
             var result = ((XolarDatabase)db).Database.ExecuteSqlCommand("dbo.InsertTraitImpact @Impact, @TraitId, @SchematicId",
                 new SqlParameter("@Impact", value),
---
+++ Xolartek.Kendo/Xolartek.Web/Areas/Fortnite/Models/TraitImpactVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Xolartek.Web.Fortnite.Models
+{
+    public class TraitImpactVM
+    {
+        public string Impact { get; set; }
+        public string Trait { get; set; }
+    }
+}
